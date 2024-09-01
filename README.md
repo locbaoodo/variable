7da7b83110d7b7e22fbdd6900416204b263ec5c1
locbaoodo <73092293+locbaoodo@users.noreply.github.com>
2024-09-01 15:50:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# variable
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/locbaoodo/variable)
---
+++ variable.js
@@ -0,0 +1,17 @@
+// TODO: Create a variable named `firstName` and assign your first name to it in the line below.
+let firstName;
+
+// TODO: Create a variable named `lastName` and assign your last name to it in the line below.
+let lastName;
+
+// TODO: Create a variable named `sayHello` and build the following string based on the variables
+// above: "Hello world! My name is <firstName> <lastName>. Nice to meet you!".
+let sayHello;
+
+// DO NOT CHANGE ANYTHING BELOW THIS LINE.
+
+module.exports = {
+  firstName,
+  lastName,
+  sayHello,
+};
